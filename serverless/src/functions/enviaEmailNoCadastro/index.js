79aedfdff801239369a8216c278e2aa9baaf1c13
JulianaAmoasei <late.parrot@gmail.com>
2023-06-14 17:47:38 -0300

    video 4.3

--- serverless/src/functions/enviaEmailNoCadastro/index.js
+++ serverless/src/functions/enviaEmailNoCadastro/index.js
@@ -1,1 +1,24 @@
-// enviaEmailNoCadastro
+const nodemailer = require('nodemailer');
+const { buildEmail } = require('../../../utils/buildEmail');
+
+const emailTransport = async () => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'elizabeth9@ethereal.email',
+      pass: 'xVqyycAu7y2s4bwaUZ'
+    }
+  });
+};
+
+module.exports.enviaEmailNoCadastro = async (evento) => {
+  const body = JSON.parse(evento.Records[0].body);
+  const transport = await emailTransport();
+  await transport.sendMail({
+    from:'',
+    to,
+    subject: '',
+    text: ''
+  });
+};
